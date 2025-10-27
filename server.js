e28e9a78a63a9c1ff0d54cabedfc9183f9eb2a9d
alaindev095 <manzialain095@gmail.com>
2025-10-27 11:45:21 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+const express = require('express');
+const app = express();
+const port = 3000;
+ 
+app.use(express.static('public'));
+
+// Simple route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
